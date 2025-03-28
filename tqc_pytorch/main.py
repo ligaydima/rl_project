26ef8a4faf78f3fffb0b49ddc59d247524abea78
Дмитрий Лигай <44410485+ligaydima@users.noreply.github.com>
2025-03-28 14:44:53 +0300

    Update main.py

--- tqc_pytorch/main.py
+++ tqc_pytorch/main.py
@@ -58,7 +58,8 @@ def main(args, results_dir, models_dir, prefix):
     for t in range(int(args.max_timesteps)):
         print(f"{state=}")
         action = actor.select_action(state)
-        next_state, reward, done, _ = env.step(action)
+        next_state, reward, done, truncated, _ = env.step(action)
+        done |= truncated
         episode_timesteps += 1
 
         replay_buffer.add(state, action, next_state, reward, done)
