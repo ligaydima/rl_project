25bd31ec6da7e70f2f03273484613c11e13dfbe1
Дмитрий Лигай <44410485+ligaydima@users.noreply.github.com>
2025-03-28 15:03:33 +0300

    Update trainer.py

--- tqc_pytorch/tqc/trainer.py
+++ tqc_pytorch/tqc/trainer.py
@@ -65,8 +65,8 @@ class Trainer(object):
 		critic_loss.backward()
 		self.critic_optimizer.step()
 
-		# for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
-		# 	target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+		for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+			target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
 		# self.actor_optimizer.zero_grad()
 		# actor_loss.backward()
