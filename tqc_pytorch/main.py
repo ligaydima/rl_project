cce9749042a86fda6da33856e0e03116fa214276
Дмитрий Лигай <44410485+ligaydima@users.noreply.github.com>
2025-03-28 16:44:26 +0300

    Update main.py

--- tqc_pytorch/main.py
+++ tqc_pytorch/main.py
@@ -75,7 +75,7 @@ def main(args, results_dir, models_dir, prefix):
             # +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
             print(f"Total T: {t + 1} Episode Num: {episode_num + 1} Episode T: {episode_timesteps} Reward: {episode_return:.3f}")
             # Reset environment
-            state, done = env.reset(), False
+            state, done = env.reset()[0], False
 
             episode_return = 0
             episode_timesteps = 0
